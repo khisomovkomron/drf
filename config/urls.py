7cd3670385d89e1481510ef588170d4901b6159e
k.hisomov <k.hisomov@bastion-tech.ru>
2022-08-03 20:49:50 +0300

    permissions/authentications/tokens

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('posts.urls'))
+    path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/dj_rest_auth/', include('dj_rest_auth.urls')),
+    path('api/v1/dj_rest_auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
